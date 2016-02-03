6fa0ce339a1e338e288a9be5308f753c45e40cee
Appledore <appledore.productions@gmail.com>
2016-02-03 17:28:14 +0000

    missed a spot

--- HolmesMVC/App_Start/AuthConfig.cs
+++ HolmesMVC/App_Start/AuthConfig.cs
@@ -7,7 +7,7 @@
         public static void RegisterAuth()
         {
             // HDB now, not AUDB
-            WebSecurity.InitializeDatabaseConnection("HolmesDBEntities", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            WebSecurity.InitializeDatabaseConnection("HolmesDB", "UserProfile", "UserId", "UserName", autoCreateTables: true);
 
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
