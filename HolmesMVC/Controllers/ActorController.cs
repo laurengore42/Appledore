fb4d0242b7c76bad130f260ecfc7ecfb4c4fe35c
Appledore <appledore.productions@gmail.com>
2016-06-03 11:29:44 +0100

    move logic into model so that local pics work everywhere, e.g. adaptation detail H+W pics

--- HolmesMVC/Controllers/ActorController.cs
+++ HolmesMVC/Controllers/ActorController.cs
@@ -30,16 +30,6 @@
 
             var actorView = new ActorView(actor);
 
-            if (actorView.Pic != null && actorView.Pic.Contains("."))
-            {
-                // it's an external URL
-            }
-            else if (actorView.Pic != null)
-            {
-                // it's an internal image name
-                actorView.Pic = @Url.Content("~/Content/Photos/Actors/") + actorView.Pic + ".jpg";
-            }
-
             return View(actorView);
         }
 
