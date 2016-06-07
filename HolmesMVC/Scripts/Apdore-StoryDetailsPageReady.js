2d14fe15886b362f99699c565349aed1f5b2bbbd
Appledore <appledore.productions@gmail.com>
2016-06-07 11:28:00 +0100

    pull all story management JS into external files

---
+++ HolmesMVC/Scripts/Apdore-StoryDetailsPageReady.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $.ajax(
+    {
+        async: true,
+        type: "GET",
+        contentType: "application/xml",
+        dataType: "text",
+        url: "/Story/StoryXml",
+        data: "storyCode=" + location.pathname.toLowerCase().replace("/story/", "").toUpperCase(),
+        success: function (content) {
+            $('.story').html(content);
+        },
+        error: function (xhr, status, errorThrown) {
+            $('.story').html(xhr.responseText);
+        }
+    });
+});
