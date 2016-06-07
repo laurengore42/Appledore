464d5477f42f4461a75314043200bc806f53a7ed
Appledore <appledore.productions@gmail.com>
2016-06-07 11:43:55 +0100

    actually implement season precision (northern hemisphere bias!)

--- HolmesMVC/Shared.cs
+++ HolmesMVC/Shared.cs
@@ -61,9 +61,32 @@
                                ? date.ToString("MMMM yyyy")
                                : date.ToString("MMM yyyy");
                 case DatePrecision.Season:
-                    if (date == new DateTime(1903, 6, 28))
+                    switch (date.Month)
                     {
-                        return "Summer 1903";
+                        case 12:
+                            return "Winter " + date.ToString("yyyy");
+                        case 1:
+                            return "Winter " + date.ToString("yyyy");
+                        case 2:
+                            return "Winter " + date.ToString("yyyy");
+                        case 3:
+                            return "Spring " + date.ToString("yyyy");
+                        case 4:
+                            return "Spring " + date.ToString("yyyy");
+                        case 5:
+                            return "Spring " + date.ToString("yyyy");
+                        case 6:
+                            return "Summer " + date.ToString("yyyy");
+                        case 7:
+                            return "Summer " + date.ToString("yyyy");
+                        case 8:
+                            return "Summer " + date.ToString("yyyy");
+                        case 9:
+                            return "Autumn " + date.ToString("yyyy");
+                        case 10:
+                            return "Autumn " + date.ToString("yyyy");
+                        case 11:
+                            return "Autumn " + date.ToString("yyyy");
                     }
 
                     return "Bad data found! 'Season' precision. Date " + date.ToString("d MMMM yyyy");
