7cf58bdf03fd929df590b11eae9fa44c9ef9de86
Appledore <appledore.productions@gmail.com>
2016-06-07 11:30:44 +0100

    reorganise, delimit shared code

--- HolmesMVC/Scripts/Apdore-StoryDetails.js
+++ HolmesMVC/Scripts/Apdore-StoryDetails.js
@@ -1,50 +1,4 @@
-﻿function htmlTrim(wholeStory) {
-    var tagless = wholeStory;
-
-    // special case for line breaks
-    var brRe = /<br.*?>/gi;
-    tagless = tagless.replace(brRe, "*¦");
-    var pRe = /<\/?p.*?>/gi;
-    tagless = tagless.replace(pRe, "*¦");
-    var divRe = /<\/?div.*?>/gi;
-    tagless = tagless.replace(divRe, "*¦");
-
-    // kill all tags
-    var re = /<.*?>/gi;
-    tagless = tagless.replace(re, "");
-
-    // kill newlines
-    tagless = tagless.replace(/\r/g, '');
-    tagless = tagless.replace(/\n{2,}/g, '*¦');
-    tagless = tagless.replace(/\n/g, '*¦');
-
-    // tidy
-    while (tagless.indexOf("*¦*¦") > -1) {
-        tagless = tagless.replace("*¦*¦", "*¦");
-    }
-    while (tagless.indexOf("*¦ ") > -1) {
-        tagless = tagless.replace("*¦ ", "*¦");
-    }
-    while (tagless.indexOf(" *¦") > -1) {
-        tagless = tagless.replace(" *¦", "*¦");
-    }
-
-    // whitespace handling
-    while (tagless.indexOf('  ') > -1) {
-        tagless = tagless.replace('  ', ' ');
-    }
-    tagless = tagless.trim();
-    
-    while (tagless.indexOf('*¦') == 0) {
-        // Sometimes this has to be done twice. Don't ask me why
-        // If you don't trim this off, you get an off-by-2 error when chunking
-        tagless = tagless.substring(2, tagless.length);
-    }
-
-    return tagless;
-}
-
-// thanks StackOverflow user karim79
+﻿// thanks StackOverflow user karim79
 function getSelectedText() {
     if (window.getSelection) {
         return window.getSelection().toString();
@@ -55,6 +9,22 @@ function getSelectedText() {
 }
 
 $(document).ready(function () {
+    $.ajax(
+    {
+        async: true,
+        type: "GET",
+        contentType: "application/xml",
+        dataType: "text",
+        url: "/Story/StoryXml",
+        data: "storyCode=" + location.pathname.toLowerCase().replace("/story/", "").toUpperCase(),
+        success: function (content) {
+            $('.story').html(content);
+        },
+        error: function (xhr, status, errorThrown) {
+            $('.story').html(xhr.responseText);
+        }
+    });
+
     $('.story').mousemove(function (event) {
         var textChunk = getSelectedText();
         var button = $('.selButton');
