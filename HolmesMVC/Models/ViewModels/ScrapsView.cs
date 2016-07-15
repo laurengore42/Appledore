c5dce41ada5621e7509c266fde3c7c4aa1df82ed
Appledore <appledore.productions@gmail.com>
2016-07-15 23:21:25 +0100

    move toys into toys folder

--- HolmesMVC/Models/ViewModels/ScrapsView.cs
+++ HolmesMVC/Models/ViewModels/ScrapsView.cs
@@ -3,6 +3,5 @@
     public class ScrapsView
     {
         public System.Collections.Generic.List<Actor> HolmesPictureList;
-        public System.Collections.Generic.List<Adaptation> AdaptationList;
     }
 }
