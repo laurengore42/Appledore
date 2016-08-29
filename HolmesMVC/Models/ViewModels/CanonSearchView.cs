3f27288020132ba00c2d612a0f4cc81297df700e
Appledore <appledore.productions@gmail.com>
2016-08-29 22:53:03 +0100

    initial phrase search implementation

---
+++ HolmesMVC/Models/ViewModels/CanonSearchView.cs
@@ -0,0 +1,26 @@
+﻿namespace HolmesMVC.Models.ViewModels
+{
+
+    using System.Collections.Generic;
+
+    public class CanonSearchView
+    {
+        public string Query;
+
+        public List<CanonSearchNode> Nodes;
+
+        public CanonSearchView()
+        {
+            Nodes = new List<CanonSearchNode>();
+        }
+
+    }
+
+    public class CanonSearchNode
+    {
+        public string Story;
+
+        public string Snippet;
+    }
+
+}
