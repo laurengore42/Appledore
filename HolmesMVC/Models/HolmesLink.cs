72f8354240e80386807dcb0e55b6d9db10d37221
Appledore <appledore.productions@gmail.com>
2016-09-22 23:47:24 +0100

    nullable airdate for when HolmesLink has an Episode

--- HolmesMVC/Models/HolmesLink.cs
+++ HolmesMVC/Models/HolmesLink.cs
@@ -14,7 +14,7 @@ namespace HolmesMVC.Models
 
         public int ID { get; set; }
         public int Episode { get; set; }
-        public System.DateTime Airdate { get; set; }
+        public System.DateTime? Airdate { get; set; }
         public string Title { get; set; }
         public int AirdatePrecision { get; set; }
         public virtual Episode Episode1 { get; set; }
