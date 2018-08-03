93046030e3c04f44afef0f58080724cbf0bc8723
AppledoreProductions <appledore.productions@gmail.com>
2018-08-03 14:15:54 +0100

    temporarily remove static, it's causing 404

--- HolmesMVC/Controllers/StoryController.cs
+++ HolmesMVC/Controllers/StoryController.cs
@@ -13,7 +13,7 @@
     {
         [AllowAnonymous]
         [HttpGet]
-        public static string StoryXml(string storyCode)
+        public string StoryXml(string storyCode)
         {
             var uri = new Uri("https://appledore.azurewebsites.net/services/storyxmlretriever.svc/retrieve?storyCode=" + storyCode);
 
