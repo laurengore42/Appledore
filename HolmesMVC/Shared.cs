e41a2b5a9fcc1f24f095e24f79b2e643647bee79
AppledoreProductions <appledore.productions@gmail.com>
2018-10-17 15:23:27 +0100

    use fallthrough

--- HolmesMVC/Shared.cs
+++ HolmesMVC/Shared.cs
@@ -37,27 +37,19 @@
                     switch (date.Month)
                     {
                         case 12:
-                            return "Winter " + date.ToString("yyyy");
                         case 1:
-                            return "Winter " + date.ToString("yyyy");
                         case 2:
                             return "Winter " + date.ToString("yyyy");
                         case 3:
-                            return "Spring " + date.ToString("yyyy");
                         case 4:
-                            return "Spring " + date.ToString("yyyy");
                         case 5:
                             return "Spring " + date.ToString("yyyy");
                         case 6:
-                            return "Summer " + date.ToString("yyyy");
                         case 7:
-                            return "Summer " + date.ToString("yyyy");
                         case 8:
                             return "Summer " + date.ToString("yyyy");
                         case 9:
-                            return "Autumn " + date.ToString("yyyy");
                         case 10:
-                            return "Autumn " + date.ToString("yyyy");
                         case 11:
                             return "Autumn " + date.ToString("yyyy");
                     }
